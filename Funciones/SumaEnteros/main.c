ffe626e2ca1487a734255d9e855648e61c98dd7a
RondonLeonR <RondonLeonR@github.com>
2018-08-29 10:57:37 -0300

    Funciones

---
+++ Funciones/SumaEnteros/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int suma(int a,int b);/***Prototipo ò Firma***/
+
+/**Funcion Principal**/
+int main()
+{
+    int nro1,nro2,result;
+    printf("\t*****SUMA******\n");
+    printf("Ingrese 1er Numero: ");
+    scanf("%d",&nro1);
+    printf("\nIngrese 2do Numero: ");
+    scanf("%d",&nro2);
+    result=suma(nro1,nro2); /***Llamada a la Funcion***/
+    printf("\n\tEl Resultado es: %d",result);
+    return 0;
+}
+
+/*****Funcion para Sumar*****/
+int suma(int a,int b)
+{
+    int result;
+    result=a+b;
+    return result;
+}
