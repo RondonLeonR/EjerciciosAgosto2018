74c96f08524101114ad29a73af2622f10f625a49
RondonLeonR <RondonLeonR>
2018-10-29 12:17:10 -0300

    adas

---
+++ ARCHIVOS/EjercicioLecturaArch/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct{
+    int legajo;
+    char nombre[20];
+    float sueldo;
+}eEmp;
+
+int main()
+{
+    FILE *pArch;
+    eEmp *newEmp;
+
+    newEmp = (eEmp*)sizeof(eEmp);
+
+    return 0;
+}
